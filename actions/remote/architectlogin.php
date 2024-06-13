e30f89ae77b77b774b5fe8165155599288aff8b0
basicprune <Firedemon198@gmail.com>
2024-06-13 14:48:30 +1000

    added a formatted login page

---
+++ actions/remote/architectlogin.php
@@ -0,0 +1,40 @@
+<?php
+
+function architectlogin_GET(Web $w)
+{
+    $w->setLayout(null);
+    
+
+    // Check if logged in already
+    $user = AuthService::getInstance($w)->user();
+    if (AuthService::getInstance($w)->loggedIn() && AuthService::getInstance($w)->allowed($user->redirect_url)) {
+        $w->redirect($w->localUrl("/virtualhome"));
+    }
+
+    $loginform = [
+        ["Architect Login ", "section", "font-size: 2rem;"],
+        ["Username", "text", "login"],
+        ["Password", "password", "password"],
+    ];
+
+    $w->ctx("loginform", Html::form($loginform, "/virtualhome-remote/architectlogin", "POST"));
+
+    
+
+}
+
+function architectlogin_POST(Web $w)
+{
+    $request_data = json_decode(file_get_contents("php://input"), true);
+   
+
+    $login = $_POST["login"];
+    $password = $_POST["password"];
+    
+
+    $user = AuthService::getInstance($w)->login($login, $password, $request_data['usertimezone'], false);
+
+    $w->redirect("/virtualhome");
+
+
+}
---
+++ templates/architectlogin.tpl.php
@@ -0,0 +1,88 @@
+<title>Architect Login YVH</title>
+<link rel="icon" type="image/x-icon" href="/uploads/virtualhomeicon.ico">
+
+<div class="floatPanel">
+
+<?php echo $loginform; ?>
+
+</div>
+<style>
+   
+    
+
+    .floatPanel{
+
+        
+
+        margin-top: 10%;
+      
+        margin-left: 30%;
+        margin-right: 25%;
+        background-color: #fefefe;
+        border: 1px solid #122648;
+        padding: 30px 50px;
+        width: 35%;
+        border-radius: 10px;
+        box-shadow: 0px 5px 10px rgba(10, 10, 10, 0.5);
+    }
+    body{
+        background-color: #34486A !important;
+    }
+    h4{
+        font-size: 1.7rem;
+    }
+    label {
+        font-size:1.2rem;
+        color: #4d4d4d;
+        cursor: pointer;
+        display: block;
+        font-weight: normal;
+        line-height: 1.5;
+        margin-bottom: 10px;
+    }
+    .savebutton{
+        text-align: center;
+        background-color: #1D84B5;
+        color: white;
+        display: inline-block;
+        border-style: solid;
+        border-width: 0;
+        cursor: pointer;
+        font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
+        font-weight: normal;
+        line-height: normal;
+        margin: 0 0 1.25rem;
+        position: relative;
+        text-decoration: none;
+        text-align: center;
+        -webkit-appearance: none;
+        border-radius: 0;
+        display: inline-block;
+        margin-top: 4;
+        padding-top: 1rem;
+        /* padding-right: 2rem; */
+        padding-bottom: 1.0625rem;
+        /* padding-left: 2rem; */
+        font-size: 1rem;
+        background-color: #008CBA;
+        border-color: #007095;
+        color: #FFFFFF;
+        transition: background-color 300ms ease-out;
+        width: 15.66667%;
+        border-radius: 5px !important;
+    
+  
+    }
+
+    html, p, span, label, h1, h2, h3, h4, h5, h6 {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif !important;
+    }
+
+    .cancelbutton{
+        visibility: hidden;
+    }
+    
+</style>
+
+
+
