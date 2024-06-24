9e8b28aeeaabb057e83cc09f1718196366ead5e0
basicprune <Firedemon198@gmail.com>
2024-06-24 13:08:25 +1000

    Update getModel.php

--- actions/remote/getModel.php
+++ actions/remote/getModel.php
@@ -17,31 +17,31 @@ function getModel_ALL(Web $w)
         $w->error('no model found for id', '/virtualhome');
     }
 
-    $downloadWindow = config::get('virtualhome.download_window');
-    $timenow = new DateTime('now', new DateTimeZone('utc'));
+    // $downloadWindow = config::get('virtualhome.download_window');
+    // $timenow = new DateTime('now', new DateTimeZone('utc'));
     
-    $dt_EndOfGenCode = $downloadcode->dt_generated->modify("+" . $downloadWindow . "hour");
+    // $dt_EndOfGenCode = $downloadcode->dt_generated->modify("+" . $downloadWindow . "hour");
     
 
     // $timeSinceGenerated = $downloadcode->dt_generated;
     if ($downloadcode->code == $downloadcode->code) 
     {
-            if ($downloadcode->dt_generated < $dt_EndOfGenCode) 
-            {
-                $w->error('Too Late ', '/virtualhome');
-            } else 
-            {
+            // if ($downloadcode->dt_generated < $dt_EndOfGenCode) 
+            // {
+            //     $w->error('Too Late ', '/virtualhome');
+            // } else 
+            // {
                 list($id) = $w->pathMatch();
 
                 $attachment = FileService::getInstance($w)->getAttachment($downloadcode->virtualhomemodel_id); // 
-                // if (!empty($attachment) && $attachment->exists()) 
-                // {
+                if (!empty($attachment) && $attachment->exists()) 
+                {
                     $attachment->writeOut();
-                // } else {
-                //     $w->header("HTTP/1.1 404 Not Found");
-                //     $w->notFoundPage();
-                // }
-            }
+                } else {
+                    $w->header("HTTP/1.1 404 Not Found");
+                    $w->notFoundPage();
+                }
+            // }
         
         
     }
